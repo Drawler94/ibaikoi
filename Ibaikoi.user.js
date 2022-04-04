e43138f7d05a3df06a119dc8bc4f081c0817a1d9
Drawler94 <102989412+Drawler94@users.noreply.github.com>
2022-04-04 19:44:58 +0200

    Add files via upload

---
+++ Ibaikoi.user.js
@@ -0,0 +1,15 @@
+
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://github.com/Drawler94/ibaikoi/blob/main/ibaikoioverlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+ 
+    }, false);
+ 
+}
